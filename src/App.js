87df3057522c7932626a768c5e077f3eede07ada
reireynoso <reireynoso@gmail.com>
2020-04-02 22:21:03 -0400

    react-redux-hooks-alternative

--- src/App.js
+++ src/App.js
@@ -38,3 +38,32 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// React-redux Hooks alternative
+
+// import React, {useEffect} from 'react'
+// import './App.css';
+// import {useSelector, useDispatch} from 'react-redux'
+// import LoginComponent from './components/LoginComponentt'
+// import SignUpComponent from './components/SignUpComponent'
+// import {autoLogin} from './actions/userActions'
+
+// const App = () => {
+//   const userReducer = useSelector(state => state.userReducer)
+//   const dispatch = useDispatch()
+//   useEffect(() => {
+//     dispatch(autoLogin())
+//   }, [])
+//   return (
+//     <div className="App">
+//         {
+//           !userReducer.loggedIn ? <h1>Sign Up or Login!</h1> : <h1>Welcome, {userReducer.user.username}</h1>
+//         }
+//         <SignUpComponent/>
+//         <LoginComponent/>
+
+//     </div>
+//   )
+// }
+
+// export default App
